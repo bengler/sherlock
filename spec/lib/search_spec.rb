72eb03111da203e248e096cb3115d5eb38ed2e25
Thomas Drevon <thomas@bengler.no>
2012-08-30 09:59:17 +0200

    Move indexing for first record to before filter, add a test.

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -14,53 +14,64 @@ describe Sherlock::Search do
     {'document' => {'app' => 'hot'}, 'realm' => realm, 'uid' => uid}
   }
 
+  before(:each) do
+    Sherlock::Search.index record
+    sleep 1.4
+  end
+
   after(:each) do
     Sherlock::Search.delete_index(realm)
   end
 
+  context "using the Query class" do
 
-  it "indexes a record" do
-    Sherlock::Search.index record
-    sleep 1.4
-    result = Sherlock::Search.query(realm, :q => "hot")
-    result['hits']['total'].should eq 1
-    result['hits']['hits'].first['_id'].should eq uid
+    it "indexes a record and finds it" do
+      query = Sherlock::Query.new("hot").to_json
+      result = Sherlock::Search.query(realm, :source => query)
+      result['hits']['total'].should eq 1
+      result['hits']['hits'].first['_id'].should eq uid
+    end
+
   end
 
-  it "does not find something thats not there" do
-    Sherlock::Search.index record
-    sleep 1.4
-    result = Sherlock::Search.query(realm, :q => "lukewarm")
-    result['hits']['total'].should eq 0
-  end    
+  context "passing an options hash directly to Search" do
 
-  it "udpates an existing record" do
-    Sherlock::Search.index record
-    update_record = {'document' => {'app' => 'lukewarm'}, 'realm' => realm, 'uid' => uid}
-    Sherlock::Search.index update_record
-    sleep 1.4
-    result = Sherlock::Search.query(realm, :q => "hot")
-    result['hits']['total'].should eq 0
-    result = Sherlock::Search.query(realm, :q => "lukewarm")
-    result['hits']['total'].should eq 1
-    result['hits']['hits'].first['_id'].should eq uid
+    it "indexes a record and finds it" do
+      result = Sherlock::Search.query(realm, :q => "hot")
+      result['hits']['total'].should eq 1
+      result['hits']['hits'].first['_id'].should eq uid
+    end
+
+    it "does not find something thats not there" do
+      result = Sherlock::Search.query(realm, :q => "lukewarm")
+      result['hits']['total'].should eq 0
+    end
+
+    it "udpates an existing record" do
+      update_record = {'document' => {'app' => 'lukewarm'}, 'realm' => realm, 'uid' => uid}
+      Sherlock::Search.index update_record
+      sleep 1.4
+      result = Sherlock::Search.query(realm, :q => "hot")
+      result['hits']['total'].should eq 0
+      result = Sherlock::Search.query(realm, :q => "lukewarm")
+      result['hits']['total'].should eq 1
+      result['hits']['hits'].first['_id'].should eq uid
+    end
+
+    it "removes index for a deleted record" do
+      Sherlock::Search.unindex record
+      sleep 1.4
+      result = Sherlock::Search.query(realm, :q => "hot")
+      result['hits']['total'].should eq 0
+    end
+
+    it "deletes the whole index" do
+      Sherlock::Search.delete_index(realm)
+      sleep 1.4
+      lambda { Sherlock::Search.query(realm, :q => "hot") }.should raise_error(Pebblebed::HttpError)
+    end
+
   end
 
-  it "removes index for a deleted record" do
-    Sherlock::Search.index record
-    sleep 1.4
-    Sherlock::Search.unindex record
-    sleep 1.4
-    result = Sherlock::Search.query(realm, :q => "hot")
-    result['hits']['total'].should eq 0
-  end
-
-  it "deletes the whole index" do
-    Sherlock::Search.index record
-    sleep 1.4
-    Sherlock::Search.delete_index(realm)
-    sleep 1.4
-    lambda { Sherlock::Search.query(realm, :q => "hot") }.should raise_error(Pebblebed::HttpError)
-  end
 
 end
