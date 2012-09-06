80effbfcaec4b3b733ecb704268f4529e1271b4d
Thomas Drevon <thomas@bengler.no>
2012-09-06 11:25:54 +0200

    Simplecov should come first.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,3 @@
-$:.unshift(File.dirname(File.dirname(__FILE__)))
-ENV['RACK_ENV'] = 'test'
-
-require 'config/environment'
-require 'sherlock'
 require 'simplecov'
 
 SimpleCov.add_filter 'spec'
@@ -10,3 +5,9 @@ SimpleCov.add_filter 'config'
 SimpleCov.add_filter 'integration'
 SimpleCov.add_filter 'coverage'
 SimpleCov.start
+
+$:.unshift(File.dirname(File.dirname(__FILE__)))
+ENV['RACK_ENV'] = 'test'
+
+require 'config/environment'
+require 'sherlock'
