6df6812d66f10a83f687dc58b759c124c24b162a
Thomas Drevon <thomas@bengler.no>
2012-09-20 15:05:59 +0200

    Fix options overwrite bug

--- lib/sherlock/indexer.rb
+++ lib/sherlock/indexer.rb
@@ -8,7 +8,7 @@ module Sherlock
     attr_reader :interval, :subscription
 
     def initialize(options = {})
-      default_options.merge! options
+      options = default_options.merge(options)
       @interval = options[:interval]
       @subscription = {:name => options[:name], :path => options[:path], :klass => options[:klass]}
     end
