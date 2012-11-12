22b431dc6e830afaae03c40f8ca6c115fbdb5e8f
Thomas Drevon <thomas@bengler.no>
2012-11-12 16:41:35 +0100

    Create missing indexes on the fly using a custom analyzer

--- lib/sherlock/elasticsearch.rb
+++ lib/sherlock/elasticsearch.rb
@@ -13,8 +13,14 @@ module Sherlock
         begin
           Pebblebed::Http.put(url(record['uid']), record)
         rescue Pebblebed::HttpError => e
-          LOGGER.warn "Error while indexing #{record['uid']}"
-          LOGGER.error e
+          if e.message =~ /IndexMissingException/
+            create_index(Pebbles::Uid.new(record['uid']).realm, index_config)
+            # now try again
+            index(record)
+          else
+            LOGGER.warn "Error while indexing #{record['uid']}"
+            LOGGER.error e
+          end
         end
       end
 
@@ -28,7 +34,7 @@ module Sherlock
       end
 
       def delete_index(realm, should_prefix_realm = true)
-        index = should_prefix_realm ? index_for(realm) : realm
+        index = should_prefix_realm ? index_name(realm) : realm
         begin
           Pebblebed::Http.delete("#{root_url}/#{index}", {})
         rescue Pebblebed::HttpError => e
@@ -39,9 +45,20 @@ module Sherlock
         end
       end
 
+      def create_index(realm, config)
+        index = index_name(realm)
+        begin
+          Pebblebed::Http.put("#{root_url}/#{index}", config)
+          LOGGER.info "Created index #{index}"
+        rescue Pebblebed::HttpError => e
+          LOGGER.warn "Unexpected error while creating index #{index}"
+          LOGGER.error e
+        end
+      end
+
       def server_status(realm = nil)
         url = "#{root_url}"
-        url << "/#{index_for(realm)}" if realm
+        url << "/#{index_name(realm)}" if realm
         url << "/_status"
         begin
           response = Pebblebed::Http.get(url, {})
@@ -55,15 +72,15 @@ module Sherlock
 
       def url(uid_string)
         uid = Pebbles::Uid.new(uid_string)
-        "#{root_url}/#{index_for(uid.realm)}/#{uid.species}/#{uid_string}"
+        "#{root_url}/#{index_name(uid.realm)}/#{uid.species}/#{uid_string}"
       end
 
-      def index_for(realm)
+      def index_name(realm)
         "#{ENV['RACK_ENV']}_#{realm}"
       end
 
       def query(realm, query_obj)
-        index = index_for(realm)
+        index = index_name(realm)
         url = "#{root_url}/#{index}/_search"
         result = nil
         options = Hash[:source => query_obj.to_json]
@@ -88,6 +105,21 @@ module Sherlock
         matching = Sherlock::Elasticsearch.query(uid.realm, query)
         return [] unless matching
         matching['hits']['hits'].map{|result| result['_id']}.compact
+      end
+
+      def index_config
+        { :settings => {
+            :analysis => {
+              :analyzer => {
+                :default => {
+                  :type => "custom",
+                  :filter => ["lowercase"],
+                  :tokenizer => "whitespace"
+                }
+              }
+            }
+          }
+        }
       end
 
     end
