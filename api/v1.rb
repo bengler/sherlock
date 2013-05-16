db03de0662557546c5433f1d51a56bf10a6ffa7d
Bjørge Næss <bjoerge@bengler.no>
2013-05-16 16:09:03 +0200

    Add headers to prevent caching in IE(9)

--- api/v1.rb
+++ api/v1.rb
@@ -11,6 +11,12 @@ class SherlockV1 < Sinatra::Base
   register Sinatra::Pebblebed
 
   before do
+    # If this service, for some reason, lives behind a proxy that rewrites the Cache-Control headers into
+    # "must-revalidate" (which IE9, and possibly other IEs, does not respect), these two headers should properly prevent
+    # caching in IE (see http://support.microsoft.com/kb/234067)
+    headers 'Pragma' => 'no-cache'
+    headers 'Expires' => '-1'
+
     cache_control :private, :no_cache, :no_store, :must_revalidate
   end
 
