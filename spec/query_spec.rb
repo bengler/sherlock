065a4ed4bc1f9683ea44f43acebdbdadf652cb82
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2014-01-09 10:21:26 +0100

    Update query order specs

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -81,11 +81,12 @@ describe Sherlock::Query do
   describe "normalizing input" do
 
     specify "sort order" do
-      Sherlock::Query.normalize_sort_order('asc').should eq 'asc'
-      Sherlock::Query.normalize_sort_order('ASC').should eq 'asc'
-      Sherlock::Query.normalize_sort_order('desc').should eq 'desc'
-      Sherlock::Query.normalize_sort_order('DESC').should eq 'desc'
-      Sherlock::Query.normalize_sort_order('anything').should eq 'desc'
+      Sherlock::Query.normalize_sort_order('asc').should eq ['asc']
+      Sherlock::Query.normalize_sort_order('ASC').should eq ['asc']
+      Sherlock::Query.normalize_sort_order('desc').should eq ['desc']
+      Sherlock::Query.normalize_sort_order('DESC').should eq ['desc']
+      Sherlock::Query.normalize_sort_order('ASC,DESC').should eq ['asc', 'desc']
+      Sherlock::Query.normalize_sort_order('anything').should eq ['desc']
     end
 
   end
