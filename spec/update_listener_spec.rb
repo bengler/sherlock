3ffb69bff9d7186f2878c819a0effb2aea88d876
Thomas Drevon <thomas@bengler.no>
2014-06-30 11:51:20 +0200

    Add a test which verifies no changed_attributes indexing

--- spec/update_listener_spec.rb
+++ spec/update_listener_spec.rb
@@ -110,4 +110,52 @@ describe Sherlock::UpdateListener do
   end
 
 
+  context 'no indexing of changed_attributes' do
+
+    let(:payload) {
+      { 'event' => 'create',
+        'uid' => 'post.card:hell.pitchfork$1',
+        'attributes' => {
+          'uid' => 'post.card:hell.pitchfork$1',
+          'document' => {'app' => 'hot'},
+          'protected' => {'price' => 'expensive'},
+          'paths' => ['hell.pitchfork'],
+          'id' => 'post.card:hell.pitchfork$1',
+          'updated_at' => '2014-06-30T10:33:04+02:00'
+        }
+      }
+    }
+
+    let(:updated_payload) {
+      { 'event' => 'update',
+        'uid' => 'post.card:hell.pitchfork$1',
+        'attributes' => {
+          'uid' => 'post.card:hell.pitchfork$1',
+          'document' => {'app' => 'hot'},
+          'protected' => {'price' => 'expensive'},
+          'paths' => ['hell.pitchfork'],
+          'id' => 'post.card:hell.pitchfork$1',
+          'updated_at' => '2014-06-30T10:46:51+02:00'
+        },
+        'changed_attributes' => {
+          "updated_at" => ["2014-06-30T10:33:04+02:00", "2014-06-30T10:46:51+02:00"]
+        }
+      }
+    }
+
+    it 'works' do
+      message1 = Hash[:payload, payload.to_json]
+      expected1 = {"uid"=>"post.card:hell.pitchfork$1", "document.app"=>"hot", "protected.price"=>"expensive", "paths"=>["hell.pitchfork"], "id"=>"post.card:hell.pitchfork$1", "updated_at"=>"2014-06-30T10:33:04+02:00", "klass_0_"=>"post", "klass_1_"=>"card", "label_0_"=>"hell", "label_1_"=>"pitchfork", "oid_"=>"1", "realm"=>"hell", "pristine"=>{"uid"=>"post.card:hell.pitchfork$1", "document"=>{"app"=>"hot"}, "protected"=>{"price"=>"expensive"}, "paths"=>["hell.pitchfork"], "id"=>"post.card:hell.pitchfork$1", "updated_at"=>"2014-06-30T10:33:04+02:00"}, "restricted"=>false}
+      Sherlock::Elasticsearch.should_receive(:index).with(expected1)
+      Sherlock::UpdateListener.new.consider message1
+
+      message2 = Hash[:payload, updated_payload.to_json]
+      expected2 = {"uid"=>"post.card:hell.pitchfork$1", "document.app"=>"hot", "protected.price"=>"expensive", "paths"=>["hell.pitchfork"], "id"=>"post.card:hell.pitchfork$1", "updated_at"=>"2014-06-30T10:46:51+02:00", "klass_0_"=>"post", "klass_1_"=>"card", "label_0_"=>"hell", "label_1_"=>"pitchfork", "oid_"=>"1", "realm"=>"hell", "pristine"=>{"uid"=>"post.card:hell.pitchfork$1", "document"=>{"app"=>"hot"}, "protected"=>{"price"=>"expensive"}, "paths"=>["hell.pitchfork"], "id"=>"post.card:hell.pitchfork$1", "updated_at"=>"2014-06-30T10:46:51+02:00"}, "restricted"=>false}
+      Sherlock::Elasticsearch.should_receive(:index).with(expected2)
+      Sherlock::UpdateListener.new.consider message2
+    end
+
+  end
+
+
 end
