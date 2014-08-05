bec8de99750f16d80dd53c9bf40c774041e8a362
Thomas Drevon <thomas@bengler.no>
2014-08-05 13:14:29 +0200

    Dont propagate broken queries to errbit

--- lib/sherlock/elasticsearch.rb
+++ lib/sherlock/elasticsearch.rb
@@ -110,12 +110,8 @@ module Sherlock
           if e.message =~ /IndexMissingException/
             LOGGER.warn "Attempt to query non-existing index: #{index} (mostly harmless)"
           else
-            if LOGGER.respond_to?:exception
-              LOGGER.exception(e)
-            else
-              LOGGER.error(e.inspect)
-              LOGGER.error(e.backtrace.join("\n"))
-            end
+            LOGGER.error(e.inspect)
+            LOGGER.error(e.backtrace.join("\n"))
           end
         end
         result
