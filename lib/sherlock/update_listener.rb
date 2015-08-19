9bca3f29941ffc958ff23ff92e2a2935d64791d3
Thomas Drevon <thomas@bengler.no>
2015-08-19 08:43:58 +0200

    Remove debug logging

--- lib/sherlock/update_listener.rb
+++ lib/sherlock/update_listener.rb
@@ -11,7 +11,6 @@ module Sherlock
     end
 
     def consider(payload)
-      LOGGER.warn("considering #{payload['uid']}")
       matching_uids = begin
         Sherlock::Elasticsearch.matching_records({'uid' => payload['uid']}.merge(payload['attributes']))
       rescue Sherlock::Elasticsearch::OldRecordError
